ec5806568a6a29320f4e90847c0e300e51798876
rheamohan <72928477+rheamohan@users.noreply.github.com>
2020-11-04 13:33:37 +0530

    Add files via upload

---
+++ 12.js
@@ -0,0 +1,56 @@
+var wall,thickness;
+var bullet,weight,speed;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness = random(22,83);
+
+  bullet = createSprite(400, 200, 50, 50);
+  bullet.shapeColor = "pink";
+  bullet.velocityX = speed;
+  bullet.weight = 40;
+
+  wall = createSprite (1200,200,20,400);
+  wall.shapeColor = "blue";
+  wall.thickness = 75;
+ 
+}
+ 
+
+function draw() {
+  background ("black"); 
+
+  
+
+  bullet.collide(wall);
+  drawSprites();
+}
+
+function hasCollided(lwall,lbullet){
+  bulletRightEdge = lbullet.x + lbullet.width;
+  wallLeftEdge = lwall.x;
+
+  if (bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false;
+}
+
+if (hasCollided (wall,bullet)){
+  bullet.velocityX = 0;
+  var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
+
+    if (damage>10){
+      wall.shapeColor = red;
+    }
+      
+      if (damage<10){
+        wall.shapeColor = green;
+      }
+
+}
+
+
